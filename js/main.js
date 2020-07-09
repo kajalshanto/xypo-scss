7f0afc7f1757f07a21ace0e747b3b83e7c0e5800
Hasibul Hasan Shanto <hasibulhasan.kajal@gmail.com>
2020-07-09 18:25:57 +0600

    initial upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Sidebar toggle
+function sideNavAnimaton() {
+    if (document.getElementById("sideNav").style.right == "-250px") {
+        document.getElementById("sideNav").style.right = "0";
+    } else {
+        document.getElementById("sideNav").style.right = "-250px";
+    }
+}
